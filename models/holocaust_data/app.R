7d94d5bac4299397c6e00358fa4764c6ac36fb71
1raghav-bhatia <1raghavbhatia@gmail.com>
2024-04-03 23:15:12 -0400

    This commits the shiny application to the github repository

--- models/holocaust_data/app.R
+++ models/holocaust_data/app.R
@@ -1,30 +1,43 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    https://shiny.posit.co/
-#
+#### Preamble ####
+# Purpose: Creates the shiny application for holocaust victims by birthplace.
+# Author: Raghav Bhatia
+# Date: 2 April 2024
+# Contact: raghav.bhatia@mail.utoronto.ca
+# License: MIT
 
 library(shiny)
+library(tidyverse)
+library(ggplot2)
+library(readr)
+
+# Load data
+Holocaust_casualties <- read_csv(
+  "data/cleaned_data/Auschwitz_Death_Certificates_1942-1943 - Auschwitz.csv")
+
+# Create a table to count victims by nationality
+casualty_count <- Holocaust_casualties |>
+  group_by(Birthplace) |>
+  summarise(Casualties = n()) |>
+  filter(Casualties >= 30)
+
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
 
     # Application title
-    titlePanel("Old Faithful Geyser Data"),
+    titlePanel("Holocaust Casualties by Nationality"),
 
     # Sidebar with a slider input for number of bins 
     sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
+      sidebarPanel(
+        selectInput(
+          inputId = "birthplaces",
+          label = "Nationality:",
+          choices = casualty_count$Birthplace,
+          multiple = TRUE,
+          selected = c("Berlin", "Amsterdam", "Hamburg", "Krakau", "Paris")
+        )
+      ),
         # Show a plot of the generated distribution
         mainPanel(
            plotOutput("distPlot")
@@ -34,18 +47,24 @@ ui <- fluidPage(
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white',
-             xlab = 'Waiting time to next eruption (in mins)',
-             main = 'Histogram of waiting times')
-    })
+  
+  selected_nationalities <- reactive({
+    casualty_count |>
+      filter(Birthplace %in% input$birthplaces)
+  })
+  
+  output$distPlot <- renderPlot({
+    # Draw the histogram with the specified number of bins
+    selected_nationalities() |>
+      ggplot(aes(x = Birthplace, y = Casualties)) +
+      geom_col() +
+      theme_minimal() +
+      scale_fill_brewer(palette = "Set1") +
+      labs(
+        x = "Nationality",
+        y = "Number of Casualties"
+      )
+  })
 }
-
 # Run the application 
 shinyApp(ui = ui, server = server)
